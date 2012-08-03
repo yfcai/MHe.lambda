070abdd8c2b9636ab53ebbed51c651528e756f9e
Cai Yufei <0.yfcai@gmail.com>
2012-08-03 08:34:54 +0200

    Type inference

--- TYP.hs
+++ TYP.hs
@@ -54,5 +54,42 @@ lconstraints term = let (a,x,c) = loop (-1) Map.empty 0 term in (a,c) where
                   (b,y,d) = loop level gamma x        t
               in (Tvar xi, y, Unify (b :> Tvar xi) a : c ++ d)
 
-linfer = error "yo"
+is_free :: Int -> Type -> Bool
+is_free a b = case b of
+ Tcon   -> False
+ Tvar i -> i == a
+ c :> d -> is_free a c || is_free a d
+
+-- substitute each occurrence of Tvar i with b in c
+tsub :: Int -> Type -> Type -> Type
+tsub i b c = case c of
+ Tcon   -> c
+ Tvar j -> if j == i then b else c
+ d :> e -> tsub i b d :> tsub i b e
+
+-- substitute each Tvar i of a with its value in tmap if exists
+tsub_all a tmap = case a of
+ Tcon   -> a
+ Tvar i -> Map.findWithDefault a i tmap
+ b :> c -> tsub_all b tmap :> tsub_all c tmap
+
+unify :: [Constraint] -> Maybe (Map.Map Int Type)
+unify cs = loop cs (Just Map.empty) where
+ loop [] ss = ss
+ loop (Unify a b : cs) ss = ss >>= \tmap -> case (a,b) of
+  (Tvar i,_)  -> if is_free i b
+                 then Nothing
+                 else loop (uclean i b cs) (tclean i b tmap)
+  (_,Tvar j)  -> if is_free j a
+                 then Nothing
+                 else loop (uclean j a cs) (tclean j a tmap)
+  (c:>d,e:>f) -> loop (Unify c e : Unify d f : cs) ss
+  otherwise   -> Nothing
+ uclean i b = map (\(Unify c d) -> Unify (tsub i b c) (tsub i b d))
+ tclean i b m = Just (Map.insert i b (Map.map (\c -> tsub i b c) m))
+
+linfer :: Llambda -> Maybe Type
+linfer term = let
+ (a, cs) = lconstraints term
+ in unify cs >>= \tmap -> return (tsub_all a tmap)
 
